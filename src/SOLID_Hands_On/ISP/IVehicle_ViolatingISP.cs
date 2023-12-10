42be017e00dca6eca9ed76f3e173a5adf381d9c5
Agustin J. Wawrzyk <76487325+agustinjosew@users.noreply.github.com>
2023-12-10 16:49:24 -0300

    Creacion de contenidos para ISP

---
+++ src/SOLID_Hands_On/ISP/IVehicle_ViolatingISP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public interface IVehicle_ViolatingISP
+    {
+        void Drive();
+        void Fly();
+    }
+
+    // In this case, the IVehicle interface violates ISP because not all vehicles can both drive and fly.
+    //
+    // For example, a bicycle can only be driven, and an airplane can only fly.
+    //
+    // Implementing the IVehicle interface in classes for these vehicles would force them to provide unnecessary and irrelevant methods.
+}
---
+++ src/SOLID_Hands_On/ISP/Vehicle_ResolvingISP.cs
@@ -0,0 +1,35 @@
+﻿using ISP.Resolving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public class Vehicle_ResolvingISP
+    {
+    }
+    // To adhere to ISP, we should create more specific interfaces tailored to each type of vehicle
+
+    public class Bicycle : IDrivable
+    {
+        public void Drive()
+        {
+            return;
+        }
+    }
+
+    public class Airplane : IFlyable
+    {
+        public void Fly()
+        {
+            return;
+        }
+    }
+
+    // By introducing separate interfaces (IDrivable and IFlyable),
+    // we adhere to ISP, ensuring that clients are not forced to implement methods they do not need.
+    // This promotes a more granular and flexible approach to interface usage and class design.
+}
